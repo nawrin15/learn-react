fcc23b21cffca371bd072260604eb2b69ad23cda
sadia9 <sadianawrin93@gmail.com>
2022-05-29 00:33:46 +0000

    add hook component

--- react-hooks/src/components/HookCounter.js
+++ react-hooks/src/components/HookCounter.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HookCounter() {
+  
+  const [count, setCounter] = useState(0)
+  
   return (
     <div>
-        <button onClick={}> Count {}</button>
+        <button onClick={()=> setCounter(count + 1)}> Count {count}</button>
     </div>
   )
 }
 
-export default HookCounter
+export default HookCounter;
